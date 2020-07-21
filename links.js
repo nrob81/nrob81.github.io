3c10d7262f16e7d8b3c3be1dc5ae7ae51e62770f
Robert Natkay <robert@nrcode.com>
2020-07-21 08:54:15 +0200

    asasas

--- links.js
+++ links.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    $('a').on('click', () => {
-        let linkPath = $(this).data('target');
-        console.log(path);
-    });
+
+	const exp = async function (path) {
+		console.log('async function...');
+		await CefSharp.BindObjectAsync("explorer");
+
+		//The default is to camel case method names (the first letter of the method name is changed to lowercase)
+		explorer.open(path).then(() => console.log('open()'))
+	}
+
+	$('a').on('click', async (event) => {
+		let linkPath = $(event.target).data('target');
+		console.log(linkPath);
+		await exp(linkPath);
+	});
 
 });
